8d8df6c405aac642a15e1aa038af907084ae570c
GrobertMendez256 <a20192256@pucp.edu.pe>
2026-01-18 19:01:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 print("Hola Git y GitHub")
+<<<<<<< HEAD
 print("Este es un nuevo cambio")
 print("Nuevo impresion en el feature")
+=======
+>>>>>>> parent of 5de4a45... Agregar segundo print de prueba
